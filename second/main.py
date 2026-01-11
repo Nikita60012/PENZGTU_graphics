d2245d41651f808cc2959d4bd098cf15d4040197
Nikita60012 <ivanov.nikita600@yandex.ru>
2026-01-11 19:20:15 +0300

    Using CV2

--- second/main.py
+++ second/main.py
@@ -1,34 +1,48 @@
-import matplotlib.pyplot as plt
+import cv2
 import numpy as np
 
-def draw(ax: plt.Axes):
-    circle_rad = [0.5, 0.3]
-    circle_y_coords = [1.3, 2.1]
-    theta = np.linspace(0, 2 * np.pi, 100)
+def draw(img: np.ndarray, width: int, height: int) -> np.ndarray:
+    lightblue = (240, 230, 200)
+    brown = (30, 70, 90)
+    black = (1, 1, 1)
 
-    bottom_circle = [0.8 * np.cos(theta), 0.8 * np.sin(theta) + 0.2]
-    ax.fill(bottom_circle[0], bottom_circle[1] * 0.8, color='lightblue', edgecolor='black')  # Заливка
+    r_bottom = 180
+    r_middle = 110
+    r_top = 60
 
-    for i in range(2):
-        x = circle_rad[i] * np.cos(theta)
-        y = circle_rad[i] * np.sin(theta) + circle_y_coords[i]
-        ax.fill(x, y, color='lightblue', edgecolor='black')
+    y_bottom = height - r_bottom
+    ellipse_height = int(r_bottom * 0.8)
+    y_middle = y_bottom - (ellipse_height + r_middle)
+    y_top = y_middle - (r_middle + r_top)
 
-    hat_width = 0.6
-    hat_height = 0.3
-    hat_x = [-hat_width / 2, hat_width / 2, hat_width / 2, -hat_width / 2, -hat_width / 2]
-    hat_y = [circle_y_coords[1] + circle_rad[1], circle_y_coords[1] + circle_rad[1],
-             circle_y_coords[1] + circle_rad[1] + hat_height,
-             circle_y_coords[1] + circle_rad[1] + hat_height,
-             circle_y_coords[1] + circle_rad[1]]
-    ax.fill(hat_x, hat_y, color='#826c34', edgecolor='black')
+    cv2.ellipse(img, (width // 2, y_bottom), (r_bottom, ellipse_height), 0, 0, 360, black, 3)
+    cv2.ellipse(img, (width // 2, y_bottom), (r_bottom, ellipse_height), 0, 0, 360, lightblue, -1)
+
+    cv2.circle(img, (width // 2, y_middle), r_middle, black, 3)
+    cv2.circle(img, (width // 2, y_middle), r_middle, lightblue, -1)
+
+    cv2.circle(img, (width // 2, y_top), r_top, black, 3)
+    cv2.circle(img, (width // 2, y_top), r_top, lightblue, -1)
+
+    hat_width = r_top * 2
+    hat_height = 60
+    hat_x1 = width // 2 - hat_width // 2
+    hat_y1 = y_top - r_top
+    hat_x2 = hat_x1 + hat_width
+    hat_y2 = hat_y1 - hat_height
+
+    cv2.rectangle(img, (hat_x1, hat_y1), (hat_x2, hat_y2), black, 3)
+    cv2.rectangle(img, (hat_x1, hat_y1), (hat_x2, hat_y2), brown, -1)
+
+    return img
 
 def main():
-    _, ax = plt.subplots(figsize=(6, 8))
-    ax.set_axis_off()
-    draw(ax)
-    ax.set_aspect('equal')
-    plt.show()
+    width, height = 800, 900
+    img = np.full((height, width, 3), 255, dtype=np.uint8)
+    snowman = draw(img, width, height)
+    cv2.imshow("Snowman", snowman)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
